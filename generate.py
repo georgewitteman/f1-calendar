f1963e00ff0471b9c6a98ed11dd84a873f96f7b9
George Witteman <george@witteman.me>
2022-05-08 19:03:16 +0000

    Use poetry instead of pip

--- generate.py
+++ generate.py
@@ -151,7 +151,9 @@ for event in original_cal.walk(name="VEVENT"):
     orig_summary = str(event["summary"]).lower()
     summary_without_prefix = orig_summary.removeprefix("formula 1").strip()
     event_name, session_name = summary_without_prefix.split(" - ", maxsplit=1)
-    event_name_without_year = event_name.removesuffix(str(event["dtstart"].dt.year)).strip()
+    event_name_without_year = event_name.removesuffix(
+        str(event["dtstart"].dt.year)
+    ).strip()
     e.add("summary", titlecase(f"{session_name} ({event_name_without_year})".lower()))
 
     # https://datatracker.ietf.org/doc/html/rfc5545#section-3.8.4.6
